2d2c65bed32cad70b381c1a4a84c8989b9060f26
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-02-17 13:55:13 -0500

    created CRUD operations

--- DevTinder_Backend/src/app.js
+++ DevTinder_Backend/src/app.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res) => {
+// this will handle only GET call to /user
+app.get("/user", (req,res) => {
+    res.send({
+        firstName : "Avyay",
+        lastName : "B"
+    });
+});
+
+app.post("/user", (req,res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Data successfully deleted");
+});
+
+app.use("/",(req,res) => {
     res.send("Hello from the server");
+});
+
+// use will match all the HTTP method API calls to /test
+app.use('/test',(req,res) => {
+    res.send("Hello the test route");
 });
 
 app.listen(3000, () => {
--- homework.md
+++ homework.md
@@ -1,3 +1,4 @@
+## Server
 - create repository
 - initialize the repository
 - node_modules, package.json, package-lock.json
@@ -10,3 +11,16 @@
 - difference between caret and tilde (^ vs ~)
 - What are dependencies
 - What is the use of `-g` while doing npm install.
+## GitHub
+- initialize git
+- gitignore
+- create a remote repo on github
+- push all the code to remote origin
+## Routes
+- play with routes and route extensions
+    - ex: /hello/2, /hell0, /test
+- Order of the routes matter a lot
+## HTTP Methods
+- Install postman app
+- Make a workspace/collection > test api calls
+- write logic to handle GET, POST, PATCH, DELETE API calls and test them on postman.
