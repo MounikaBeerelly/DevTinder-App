15a5c9f5e8e17c6566d153e87bc41ce79d0ae063
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-02-21 15:31:03 -0500

    Redirect to login if token not present

--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,10 +1,10 @@
-import Body from "./Body";
-import Login from "./Login";
+import Body from "./components/Body";
+import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Profile from "./Profile";
+import Profile from "./components/Profile";
 import { Provider } from 'react-redux';
 import appStore from "./utils/appStore";
-import Feed from "./Feed";
+import Feed from "./components/Feed";
 
 function App() {
   return (
